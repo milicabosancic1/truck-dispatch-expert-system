2861071fc303891609157ffd2221b607368a0ac5
milicabosancic1 <milicabosancic1612@gmail.com>
2026-05-16 14:02:52 +0200

    feat: add Drools and project dependencies

---
+++ truck-dispatch/truck-dispatch/src/main/java/com/truckdispatch/truck_dispatch/TruckDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.truckdispatch.truck_dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckDispatchApplication.class, args);
+	}
+
+}
---
+++ truck-dispatch/truck-dispatch/src/test/java/com/truckdispatch/truck_dispatch/TruckDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truckdispatch.truck_dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
